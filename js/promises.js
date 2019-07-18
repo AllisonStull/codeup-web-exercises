cb82be9e432691d5ed49e579d727707e7138eeb5
Allison Stull <allisonstull3@gmail.com>
2019-07-17 20:15:00 -0500

    js/promises/media queries

---
+++ js/promises.js
@@ -0,0 +1,10 @@
+// Exercises promises
+// Create a file named promises.js inside of your js directory.
+//
+//     Write a function named wait that accepts a number as a parameter, and returns a promise that resolves after the passed number of milliseconds.
+function wait(){
+
+}
+
+wait(1000).then(() => console.log('You\'ll see this after 1 second'));
+wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
